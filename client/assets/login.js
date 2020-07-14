78b3d2f8c4df68ac71a909584ee0e18923867b44
Philip Kouchner <philipk179@gmail.com>
2020-07-13 22:21:45 -0700

    fixing the login page

--- client/assets/login.js
+++ client/assets/login.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
     const username = $("#username");
     const password = $("#password");
-    const UserLogin = {
-        username: username.val(),
-        password: password.val(),
-    }
+
     $("#btn_login").on("click", function (event) {
         event.preventDefault();
+        const UserLogin = {
+            username: username.val(),
+            password: password.val(),
+        }
         $.ajax({
             type: "POST",
             url: "/api/login",
             data: UserLogin
         }).then(res => {
             console.log(res);
-            // window.location.href = "/dashboard.html";
+            window.location.href = "/dashboard.html";
         }).catch(err => console.log(err));
+    });
+    $("#btn_return").on("click", function (event) {
+        event.preventDefault();
+        window.location.href = "index.html";
     });
 });
 
